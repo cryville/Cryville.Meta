6b21ea615e5ee3ace0ab8de0e4169716d7eb235c
PopSlime <popslime@qq.com>
2023-10-20 11:47:29 +0800

    Rearrange the data structure of the meton pair model.

--- Cryville.Meta/Model/MetonPairModel.cs
+++ Cryville.Meta/Model/MetonPairModel.cs
@@ -2,29 +2,29 @@ using System;
 using System.IO;
 
 namespace Cryville.Meta.Model {
-	internal struct MetonPair : IModel, IComparable<MetonPair> {
-		public MetonIdentifier Key;
+	internal struct MetonPairModel : IModel, IComparable<MetonPairModel> {
 		public ulong KeyPointer;
-		public MetonIdentifier Value;
+		public MetonIdentifier Key;
 		public ulong ValuePointer;
+		public MetonIdentifier Value;
 
-		public int CompareTo(MetonPair other) {
+		public readonly int CompareTo(MetonPairModel other) {
 			int r = Key.CompareTo(other.Key);
 			if (r != 0) return r;
 			return Value.CompareTo(other.Value);
 		}
 
 		public void ReadFrom(BinaryReader reader) {
-			Key = reader.ReadModel<MetonIdentifier>();
 			KeyPointer = reader.ReadUInt64();
-			Value = reader.ReadModel<MetonIdentifier>();
+			Key = reader.ReadModel<MetonIdentifier>();
 			ValuePointer = reader.ReadUInt64();
+			Value = reader.ReadModel<MetonIdentifier>();
 		}
-		public void WriteTo(BinaryWriter writer) {
-			writer.Write(Key);
+		public readonly void WriteTo(BinaryWriter writer) {
 			writer.Write(KeyPointer);
-			writer.Write(Value);
+			writer.Write(Key);
 			writer.Write(ValuePointer);
+			writer.Write(Value);
 		}
 	}
 }
