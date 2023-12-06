3193575bb645ad03b4ab02d556980cf8b6780278
PopSlime <popslime@qq.com>
2023-12-06 15:51:12 +0800

    Implement IEquatable and operators on MetonIdentifier.

--- Cryville.Meta/Model/MetonIdentifier.cs
+++ Cryville.Meta/Model/MetonIdentifier.cs
@@ -2,7 +2,7 @@ using System;
 using System.IO;
 
 namespace Cryville.Meta.Model {
-	public struct MetonIdentifier : IModel, IComparable<MetonIdentifier> {
+	public struct MetonIdentifier : IModel, IComparable<MetonIdentifier>, IEquatable<MetonIdentifier> {
 		public ulong TypeKey { get; set; }
 		public ulong SubKey1 { get; set; }
 		public ulong SubKey2 { get; set; }
@@ -45,5 +45,41 @@ namespace Cryville.Meta.Model {
 			writer.Write(SubKey3);
 			writer.Write(SubKey4);
 		}
+
+		/// <inheritdoc />
+		public readonly bool Equals(MetonIdentifier other) =>
+			TypeKey == other.TypeKey &&
+			SubKey1 == other.SubKey1 &&
+			SubKey2 == other.SubKey2 &&
+			SubKey3 == other.SubKey3 &&
+			SubKey4 == other.SubKey4;
+		/// <inheritdoc />
+		public override readonly bool Equals(object obj) => obj is MetonIdentifier other && Equals(other);
+
+		/// <inheritdoc />
+		public override readonly int GetHashCode() {
+			var ret = TypeKey.GetHashCode();
+			ret ^= SubKey1.GetHashCode();
+			var h2 = SubKey2.GetHashCode();
+			ret ^= h2 << 8 | h2 >> 24;
+			var h3 = SubKey3.GetHashCode();
+			ret ^= h3 << 16 | h3 >> 16;
+			var h4 = SubKey4.GetHashCode();
+			ret ^= h4 << 24 | h4 >> 8;
+			return ret;
+		}
+
+		/// <inheritdoc />
+		public static bool operator ==(MetonIdentifier left, MetonIdentifier right) => left.Equals(right);
+		/// <inheritdoc />
+		public static bool operator !=(MetonIdentifier left, MetonIdentifier right) => !(left == right);
+		/// <inheritdoc />
+		public static bool operator <(MetonIdentifier left, MetonIdentifier right) => left.CompareTo(right) < 0;
+		/// <inheritdoc />
+		public static bool operator <=(MetonIdentifier left, MetonIdentifier right) => left.CompareTo(right) <= 0;
+		/// <inheritdoc />
+		public static bool operator >(MetonIdentifier left, MetonIdentifier right) => left.CompareTo(right) > 0;
+		/// <inheritdoc />
+		public static bool operator >=(MetonIdentifier left, MetonIdentifier right) => left.CompareTo(right) >= 0;
 	}
 }
